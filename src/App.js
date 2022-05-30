6ee4120f3146c2fc89572559a96068462d86ccaf
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-05-30 18:34:16 +0200

    Installed project dependencies

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Airbnb</h1>
+      <h1 className="text-3xl font-bold underline">Airbnb</h1>
     </div>
   );
 }
