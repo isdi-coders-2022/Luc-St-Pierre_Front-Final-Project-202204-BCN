795a9a0ea60b3a1d2b199cf80bc25a2466880704
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-05-31 23:38:45 +0200

    Re-added App.test.tsx but added BrowserRouter that was causing error

---
+++ src/App.test.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+
+test("renders Airbnb", () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  const linkElement = screen.getByText(/homepage/i);
+  expect(linkElement).toBeInTheDocument();
+});
