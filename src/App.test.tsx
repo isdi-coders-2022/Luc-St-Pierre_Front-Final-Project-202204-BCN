89db0816fea4e6b099665f9b79e2b7c50de4d772
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-05-31 22:33:33 +0200

    Removed default app.test.tsx

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders Airbnb", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Airbnb/i);
-  expect(linkElement).toBeInTheDocument();
-});
