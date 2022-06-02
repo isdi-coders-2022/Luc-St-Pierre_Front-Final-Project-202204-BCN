48a13ca1247de97f3d9bd6485611cd14050ff561
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-06-02 12:19:59 +0200

    Updated test to use Provider

--- src/components/LoginForm/LoginForm.test.tsx
+++ src/components/LoginForm/LoginForm.test.tsx
@@ -1,32 +1,28 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { store } from "../../redux/store/store";
+
 import LoginForm from "./LoginForm";
 
 describe("Given a LoginForm component", () => {
-  describe("When the user write 'Vero' and'passwordX'for name, username and password", () => {
-    test("Then it should fill the 2 inputs with the values of 'Vero' and'passwordX'", () => {
-      const username = "Vero";
-      const password = "passwordY";
-      const textButton = "Sign in";
+  describe("When the user write 'Vero' in the input field for useranme", () => {
+    test("Then it should fill the input username with 'Vero'", () => {
+      const labelToFind = "Username";
+      const inputTextValue = "Vero";
       render(
         <BrowserRouter>
-          <LoginForm />
+          <Provider store={store}>
+            <LoginForm />
+          </Provider>
         </BrowserRouter>
       );
 
-      const expectedElement1 = screen.queryByTestId("inputUsername");
-      const expectedElement2 = screen.queryByTestId("inputPassword");
+      const label: HTMLLabelElement = screen.getByLabelText(labelToFind);
+      userEvent.type(label, inputTextValue);
 
-      userEvent.type(expectedElement1 as HTMLElement, username);
-      userEvent.type(expectedElement2 as HTMLElement, password);
-
-      const expectedButton = screen.getByText(textButton);
-      userEvent.click(expectedButton);
-
-      expect(expectedElement1).toHaveValue(username);
-      expect(expectedElement2).toHaveValue(password);
-      expect(expectedButton).not.toBeDisabled();
+      expect(label).toHaveValue(inputTextValue);
     });
   });
 });
