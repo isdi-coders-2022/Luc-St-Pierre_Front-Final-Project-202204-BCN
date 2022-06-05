ec4751173cfe03bbb7de31bcbef0f56065fc3c9b
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-06-05 20:21:18 +0200

    Added rating icon

--- src/components/PlacesList/PlacesList.tsx
+++ src/components/PlacesList/PlacesList.tsx
@@ -8,7 +8,7 @@ interface Props {
 const PlacesList = ({ showAllPlaces }: Props) => {
   return (
     <section aria-labelledby="related-heading" className="mt-16 sm:mt-24">
-      <ul className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-6">
+      <ul className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-6">
         {showAllPlaces.map((place) => {
           console.log(place);
           return (
