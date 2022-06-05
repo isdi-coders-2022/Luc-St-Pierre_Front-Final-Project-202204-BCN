d8376110b318a2cb8ff7ff8c6f2bc0b9288c42ab
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-06-05 13:32:46 +0200

    Added placesReducer to store

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,12 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import placesReducer from "../reducers/features/placesSlice/placesSlice";
 import userReducer from "../reducers/features/userSlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    places: placesReducer,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
-
 export type AppDispatch = typeof store.dispatch;
