1b74bdbe3a2d141fa36908c36f5729c1f5ad1c42
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-06-06 13:27:21 +0200

    Created test for rendering list of places

---
+++ src/components/PlacesList/PlacesList.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { placesMock } from "../../mocks/placesMocks";
+import { store } from "../../redux/store/store";
+import PlacesList from "./PlacesList";
+
+describe("Given a PlacesList component", () => {
+  describe("When it's rendered with a list of 1 place", () => {
+    test("Then it should return", () => {
+      const expectedNumberOfPlaces = 1;
+
+      render(
+        <BrowserRouter>
+          <Provider store={store}>
+            <PlacesList showAllPlaces={placesMock} />
+          </Provider>
+        </BrowserRouter>
+      );
+
+      const listElements = screen.getAllByRole("listitem");
+      expect(listElements).toHaveLength(expectedNumberOfPlaces);
+    });
+  });
+});
