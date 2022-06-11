ba9afcffeafcb51d2f387efde5d6b7600362509b
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-06-11 12:21:06 +0200

    Created loadPlaceThunk middleware

--- src/pages/PlaceDetailPage.tsx
+++ src/pages/PlaceDetailPage.tsx
@@ -1,4 +1,27 @@
+import { useParams } from "react-router-dom";
+import { useAppSelector } from "../redux/store/hooks";
+
 const PlaceDetailPage = () => {
+  const { placeId } = useParams();
+
+  const {
+    title,
+    description,
+    imageBackup,
+    address,
+    city,
+    placeType,
+    placeDescription,
+    price,
+    numberOfRooms,
+    numberOfBeds,
+    numberOfGuests,
+    country,
+    rating,
+    kilometers,
+    category,
+  } = useAppSelector((state) => state.place);
+
   return <div>PlaceDetailPage</div>;
 };
 
