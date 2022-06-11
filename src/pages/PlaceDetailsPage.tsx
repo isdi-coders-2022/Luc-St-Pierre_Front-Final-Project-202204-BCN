4c2c5ce5cd0690008aad44a66676b61250a73daf
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-06-11 16:26:58 +0200

    Added container and two columns, and title and description

--- src/pages/PlaceDetailsPage.tsx
+++ src/pages/PlaceDetailsPage.tsx
@@ -26,7 +26,11 @@ const PlaceDetailsPage = () => {
       <div className="mt-6 max-w-7xl grid grid-cols-4 gap-x-2">
         <div className="max-h-[413px] col-span-2 aspect-w-3 aspect-h-8 overflow-hidden block rounded-tl-lg rounded-bl-lg">
           <img
-            src="https://firebasestorage.googleapis.com/v0/b/airbnb-eed3e.appspot.com/o/1654881776507-design_photo_1_2.webp?alt=media&token=0e4f42d3-29e3-4f92-817e-0f6827ad9d46"
+            src={
+              place.imageBackup
+                ? place.imageBackup
+                : "/images/generic-record.png"
+            }
             alt="Two each of gray, white, and black shirts laying flat."
             className="w-full h-full object-center object-cover"
           />
@@ -65,6 +69,77 @@ const PlaceDetailsPage = () => {
             />
           </div>
         </div>
+      </div>
+
+      <div className="flex-1 flex items-stretch">
+        <main className="flex-1">
+          <section className="min-w-0 flex-1 h-full flex flex-col lg:order-last pt-12">
+            <div className="h-full flex items-center">
+              <div className="pr-6 h-full">
+                <div className="block rounded h-full w-full text-[#222222]">
+                  <h2 className="text-[22px] font-semibold">
+                    Private room in island hosted by The Arctic Hideaway
+                  </h2>
+
+                  <ol className="flex space-x-1">
+                    <li className="inline-flex items-center text-sm">
+                      <span className="text-base font-medium text-gray-900">
+                        {place.numberOfGuests}
+                      </span>
+                      <span className="text-base font-normal text-gray-900 ml-1">
+                        guest
+                      </span>
+                    </li>
+                    <span>·</span>
+                    <li className="inline-flex items-center text-sm">
+                      <span className="text-base font-medium text-gray-900">
+                        {place.numberOfRooms}
+                      </span>
+                      <span className="text-base font-normal text-gray-900 ml-1">
+                        bedroom
+                      </span>
+                    </li>
+                    <span>·</span>
+                    <li className="inline-flex items-center text-sm">
+                      <span className="text-base font-medium text-gray-900">
+                        {place.numberOfBeds}
+                      </span>
+                      <span className="text-base font-normal text-gray-900 ml-1">
+                        bed
+                      </span>
+                    </li>
+                  </ol>
+                </div>
+              </div>
+
+              <div className="h-full">
+                <div className="block rounded h-full w-full text-[#222222]">
+                  <img src="/assets/avatar.png" alt="" className="h-14 w-14" />
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <section className="min-w-0 flex-1 h-full flex flex-col lg:order-last pt-12">
+            <div className="h-full flex items-center">
+              <div className="pr-6 h-full">
+                <div className="block rounded h-full w-full text-[#222222]">
+                  <p className="text-base">{place.description}</p>
+                </div>
+              </div>
+            </div>
+          </section>
+        </main>
+
+        <aside className="hidden w-96 bg-white overflow-y-auto lg:block">
+          <div className="h-full">
+            <div className="p-6 h-full">
+              <div className="block rounded h-full w-full text-gray-200">
+                Aside
+              </div>
+            </div>
+          </div>
+        </aside>
       </div>
     </>
   );
