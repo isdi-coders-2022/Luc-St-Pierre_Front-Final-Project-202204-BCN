9bf578036bfe00a206c611298b4c01f3db947847
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-06-12 22:14:32 +0200

    Added Places MapLeaflet

--- src/components/MapLeaflet/MapLeaflet.tsx
+++ src/components/MapLeaflet/MapLeaflet.tsx
@@ -1,16 +1,24 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { IPlace } from "../../types/places.types";
 import "./MapLeaflet.css";
 
-const MapLeaflet = () => {
+interface Props {
+  places: IPlace[];
+}
+
+const MapLeaflet = ({ places }: Props) => {
   return (
     <MapContainer center={[50, 15]} zoom={3} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>A pretty CSS3 popup. Easily customizable.</Popup>
-      </Marker>
+
+      {places.map((place: IPlace, index) => (
+        <Marker key={index} position={[41.3874, 2.1686]}>
+          <Popup>A pretty CSS3 popup. Easily customizable.</Popup>
+        </Marker>
+      ))}
     </MapContainer>
   );
 };
