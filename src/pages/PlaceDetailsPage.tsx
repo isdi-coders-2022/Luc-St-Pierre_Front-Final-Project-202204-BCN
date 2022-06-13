4160544c8c1b19fe55fb3e38f78c632d385d1b0a
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-06-13 10:00:16 +0200

    Added test for placeDetailsPage

--- src/pages/PlaceDetailsPage.tsx
+++ src/pages/PlaceDetailsPage.tsx
@@ -48,14 +48,12 @@ const PlaceDetailsPage = () => {
                 <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
               </svg>
               <span className="font-medium text-gray-900">4.8</span>
-              <span className="sr-only">likes</span>
             </button>
           </span>
           <span className="inline-flex items-center text-sm">
             <span className="font-medium text-[#222222] underline">
               {place.city}, {place.country}
             </span>
-            <span className="sr-only">replies</span>
           </span>
         </div>
       </section>
