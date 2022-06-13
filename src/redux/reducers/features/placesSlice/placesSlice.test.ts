33a448dfe78c994f153bb34fbe3865a9159a4e7b
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-06-13 10:55:13 +0200

    Added test for updatePlace

--- src/redux/reducers/features/placesSlice/placesSlice.test.ts
+++ src/redux/reducers/features/placesSlice/placesSlice.test.ts
@@ -3,6 +3,8 @@ import { placesMock } from "../../../../mocks/placesMocks";
 import placesReducer, {
   loadPlacesActionCreator,
   addPlaceActionCreator,
+  deletePlaceActionCreator,
+  updatePlaceActionCreator,
 } from "./placesSlice";
 
 describe("Given a placesReducer reducer", () => {
@@ -57,6 +59,65 @@ describe("Given a placesReducer reducer", () => {
       const { places } = placesReducer(initialState, action);
 
       expect(places).toHaveLength(expectedLength);
+    });
+  });
+
+  describe("When it's invoked with a deletePlace action with an id and an initial state with 2 places", () => {
+    test("Then it should return 1 places to the places array", async () => {
+      const expectedLength = 2;
+      const action = deletePlaceActionCreator(placesMock[0].creator);
+
+      const initialState = {
+        places: placesMock,
+        pages: 0,
+        currentPage: 1,
+        perPageCount: 8,
+      };
+
+      const { places } = placesReducer(initialState, action);
+
+      expect(places).toHaveLength(expectedLength);
+    });
+  });
+
+  describe("When it's invoked with a updatePlace action with 2 places as an initial state and a update place", () => {
+    test("Then it should update the place and change it's title", async () => {
+      const updatedNote = {
+        title: "Canada places",
+        description: "This is a description",
+        image: "abcd.jpeg",
+        address: "34 Llurai",
+        city: "Mongat",
+        placeType: "House",
+        placeDescription: "casa",
+        price: "86",
+        numberOfRooms: "3",
+        numberOfBeds: "2",
+        numberOfGuests: "3",
+        country: "Canada",
+        creator: "629b88b726b95714b076c60a",
+        rating: "4.88",
+        isListed: true,
+        kilometers: "343",
+        category: "National parks",
+        location: {
+          type: "Point",
+          coordinates: [41.3874, 2.1686],
+        },
+      };
+
+      const action = updatePlaceActionCreator(updatedNote);
+
+      const initialState = {
+        places: placesMock,
+        pages: 0,
+        currentPage: 1,
+        perPageCount: 8,
+      };
+
+      const { places } = placesReducer(initialState, action);
+
+      expect(places[0]).toHaveProperty("title", updatedNote.title);
     });
   });
 });
