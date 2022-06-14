223fb393b7fede4f3b5720fe3970232351e725fb
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-06-15 00:25:50 +0200

    Added Categories component to MapLeaflet component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,5 @@
 import { useLocation } from "react-router-dom";
 import { useAppSelector } from "../../redux/store/hooks";
-// import Categories from "../Categories/Categories";
 import Navigation from "../Navigation/Navigation";
 
 interface Props {
@@ -14,7 +13,6 @@ const Layout = ({ children }: Props): JSX.Element => {
   return (
     <>
       <Navigation userData={userData} />
-      {/* {location.pathname === "/hosts/home" ? <Categories /> : ""} */}
       <div
         className={
           location.pathname === "/hosts/home"
