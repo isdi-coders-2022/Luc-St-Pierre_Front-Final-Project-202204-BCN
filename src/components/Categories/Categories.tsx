3f704a5ff18ab906b49b9211ae708593be482068
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-06-14 17:24:45 +0200

    Fixed categories overflow scroll

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -16,12 +16,12 @@ const Categories = <
   return (
     <div className="absolute right-0 left-0 top-[5.5rem] bg-white shadow z-[99]">
       <div className="flex items-center justify-between max-w-full sm:px-6 md:px-10 xl:px-20">
-        <div className="flex justify-between h-[78px] max-w-[1440px] space-x-10">
+        <div className="flex justify-between h-[78px] max-w-[1440px] space-x-10 overflow-x-auto">
           {categories.map((category, index) => {
             return (
               <button
                 key={index}
-                className="flex flex-col items-center justify-center pt-1"
+                className="flex flex-col items-center flex-shrink-0 justify-center pt-1"
                 onClick={() => onSelectCategory(category)}
               >
                 <span className="flex flex-col justify-center items-center">
